6b5fc8be0806b89b7ba1ec0605310478d0adbfad
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-05-17 18:30:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,18 @@
 class Song
   
-  attr_accessor :artist, :name
-  
+  attr_accessor :name, :artist
+
   def initialize(name)
     @name = name
   end
-  
+
+  def artist_name
+  if @artist
+      @artist.name
+    else
+      nil
+    end
+  end
+
   
 end
